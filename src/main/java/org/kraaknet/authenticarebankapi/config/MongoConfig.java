e865c8526753aca11b6d0cca199b05d3feb18c79
Floris Kraak <floris.kraak@booking.com>
2023-10-19 14:36:03 +0200

    Use `@EnableMongoRepositories`
    That Baeldung site doesn't seem to know how it wants to set this up ..

--- src/main/java/org/kraaknet/authenticarebankapi/config/MongoConfig.java
+++ src/main/java/org/kraaknet/authenticarebankapi/config/MongoConfig.java
@@ -5,22 +5,18 @@ import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import io.micrometer.common.lang.NonNullApi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-import java.util.Collection;
-import java.util.Collections;
-
-@Configuration
 @NonNullApi
-public class MongoConfig extends AbstractMongoClientConfiguration {
+@Configuration
+@EnableMongoRepositories(basePackages = "org.kraaknet.authenticarebankapi.repository.database.model")
+public class MongoConfig {
 
-    @Override
-    protected String getDatabaseName() {
-        return "authenticarebank";
-    }
-
-    @Override
+    @Bean
     public MongoClient mongoClient() {
         var connectionString = new ConnectionString("mongodb://localhost:27017/authenticarebank");
         var mongoClientSettings = MongoClientSettings.builder()
@@ -30,8 +26,10 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
         return MongoClients.create(mongoClientSettings);
     }
 
-    @Override
-    public Collection<String> getMappingBasePackages() {
-        return Collections.singleton("org.kraaknet.authenticarebankapi.repository.database.model");
+    @Bean
+    @Autowired
+    public MongoTemplate mongoTemplate(MongoClient client) {
+        return new MongoTemplate(client, "test");
     }
+
 }
