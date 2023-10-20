d15112ec5872ad9f671fa859451756cfe074aae0
Floris Kraak <floris.kraak@booking.com>
2023-10-20 17:15:07 +0200

    Add some logging for the customer requests

--- src/main/java/org/kraaknet/authenticarebankapi/controller/CustomerController.java
+++ src/main/java/org/kraaknet/authenticarebankapi/controller/CustomerController.java
@@ -1,17 +1,17 @@
 package org.kraaknet.authenticarebankapi.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.kraaknet.authenticarebankapi.controller.api.CustomerApi;
 import org.kraaknet.authenticarebankapi.controller.model.CustomerDto;
 import org.kraaknet.authenticarebankapi.service.CustomerService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @Validated
-@RequestMapping("${openapi.api.base-path:}")
 @RequiredArgsConstructor
 public class CustomerController implements CustomerApi {
 
@@ -21,11 +21,13 @@ public class CustomerController implements CustomerApi {
     @Override
     public ResponseEntity<CustomerDto> getCurrentCustomer() {
         // Todo - get that id from the authentication context
+        log.info("getCurrentCustomer()");
         return ResponseEntity.of(customerService.findCustomerById(1));
     }
 
     @Override
     public ResponseEntity<CustomerDto> getCustomerById(Long id) {
+        log.info("getCustomerById({})", id);
         return ResponseEntity.of(customerService.findCustomerById(id));
     }
 
