70b43f213dcf1bb13a2bb3aabed119e703786d0f
Floris Kraak <floris.kraak@booking.com>
2023-10-20 09:19:25 +0200

    Add simple User entity

---
+++ src/main/java/org/kraaknet/authenticarebankapi/repository/database/model/User.java
@@ -0,0 +1,32 @@
+package org.kraaknet.authenticarebankapi.repository.database.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user")
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    // Todo: Nullable, and format verification
+    @Column(name = "email")
+    private String email;
+}
