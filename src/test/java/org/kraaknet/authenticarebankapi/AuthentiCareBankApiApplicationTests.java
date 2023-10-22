710885eca66345083e46175f39c38d231f623a10
Floris Kraak <floris.kraak@booking.com>
2023-10-22 17:12:20 +0200

    Add postgres container to the AuthentiCareBankApiApplicationTests

--- src/test/java/org/kraaknet/authenticarebankapi/AuthentiCareBankApiApplicationTests.java
+++ src/test/java/org/kraaknet/authenticarebankapi/AuthentiCareBankApiApplicationTests.java
@@ -3,8 +3,13 @@ package org.kraaknet.authenticarebankapi;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import static java.lang.Boolean.TRUE;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -12,7 +17,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration
+@Testcontainers
 class AuthentiCareBankApiApplicationTests {
+
+    @Container
+    @ServiceConnection
+    private static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer(DockerImageName.parse("postgres:13.1-alpine"));
 
     @Test
     void testContextLoads() {
