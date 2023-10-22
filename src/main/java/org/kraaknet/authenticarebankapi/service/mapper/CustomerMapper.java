08ef7d4a9371f3b8fb2ca16a17d8c9c80d07d8db
Floris Kraak <floris.kraak@booking.com>
2023-10-22 20:57:18 +0200

    Tell Mapstruct to ignore some fields when mapping

--- src/main/java/org/kraaknet/authenticarebankapi/service/mapper/CustomerMapper.java
+++ src/main/java/org/kraaknet/authenticarebankapi/service/mapper/CustomerMapper.java
@@ -4,6 +4,7 @@ import org.kraaknet.authenticarebankapi.controller.model.CustomerModel;
 import org.kraaknet.authenticarebankapi.controller.model.CustomerViewModel;
 import org.kraaknet.authenticarebankapi.repository.database.model.CustomerEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
 
@@ -15,7 +16,8 @@ public interface CustomerMapper  {
 
     CustomerViewModel toViewModel(CustomerEntity customerEntity);
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "accounts", ignore = true)
     CustomerEntity toEntity(CustomerModel model);
 
-    CustomerEntity toEntity(CustomerViewModel model);
 }
