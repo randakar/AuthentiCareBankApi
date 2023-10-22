167fb8d4d2d8f1dc17b2beb8c65409549e4cbf01
Floris Kraak <floris.kraak@booking.com>
2023-10-22 17:34:32 +0200

    Setup MockMvc for Spring Boot integration testing

--- src/test/java/org/kraaknet/authenticarebankapi/AuthentiCareBankApiApplicationTests.java
+++ src/test/java/org/kraaknet/authenticarebankapi/AuthentiCareBankApiApplicationTests.java
@@ -1,11 +1,17 @@
 package org.kraaknet.authenticarebankapi;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -13,8 +19,10 @@ import org.testcontainers.utility.DockerImageName;
 
 import static java.lang.Boolean.TRUE;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration
 @Testcontainers
@@ -24,10 +32,30 @@ class AuthentiCareBankApiApplicationTests {
     @ServiceConnection
     private static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>(DockerImageName.parse("postgres:13.1-alpine"));
 
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @BeforeEach
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
     @Test
     void testContextLoads() {
         // No errors so far, so application started successfully
         assertTrue(TRUE);
     }
+
+    @Test
+    @WithMockUser(username = "john", roles = { "ADMIN" })
+    void givenRoleViewer_whenCallGetUsername_thenReturnUsername() {
+
+    }
+
 
 }
