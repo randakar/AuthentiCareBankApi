3c7627e5e4113c78936f8df4a8757ed15de21fa9
Floris Kraak <floris.kraak@booking.com>
2023-10-22 16:14:03 +0200

    Implement exception handling for NotAuthorizedException

--- src/main/java/org/kraaknet/authenticarebankapi/controller/ExceptionResponseHandler.java
+++ src/main/java/org/kraaknet/authenticarebankapi/controller/ExceptionResponseHandler.java
@@ -2,11 +2,10 @@ package org.kraaknet.authenticarebankapi.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.checkerframework.checker.nullness.qual.NonNull;
-import org.springframework.beans.TypeMismatchException;
+import org.kraaknet.authenticarebankapi.controller.exceptions.NotAuthorizedException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -19,16 +18,16 @@ public class ExceptionResponseHandler extends ResponseEntityExceptionHandler {
     /**
      * Handlers like this should be there to prevent security sensitive details about the used libs to leak to the user.
      *
-     * @param ex Thrown type exception
+     * @param e Thrown type exception
      * @param request Triggering request
      * @return ResponseEntity with the details that can be given back safely to the end user.
      */
-    @ExceptionHandler(value = TypeMismatchException.class)
-    public ResponseEntity<Object> typeMismatchExceptionHandler(@NonNull TypeMismatchException ex,
+    @ExceptionHandler(value = NotAuthorizedException.class)
+    public ResponseEntity<Object> notAuthorizedExceptionHandler(@NonNull NotAuthorizedException e,
                                                                @NonNull WebRequest request) {
-        log.warn("typeMismatchExceptionHandler({}, {})", ex, request);
+        log.warn("notAuthorizedExceptionHandler({}, {})", e, request);
 
-        var message = "Type mismatch in request.";
-        return handleExceptionInternal(ex, message, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+        var message = "Not authorized";
+        return handleExceptionInternal(e, message, new HttpHeaders(), HttpStatus.UNAUTHORIZED, request);
     }
 }
