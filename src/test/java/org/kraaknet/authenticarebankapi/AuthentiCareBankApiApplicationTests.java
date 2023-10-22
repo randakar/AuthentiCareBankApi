f6d62e245dba055e5cc5ee41394992d173dc1b8c
Floris Kraak <floris.kraak@booking.com>
2023-10-22 16:39:34 +0200

    Add security config to AuthentiCareBankApiApplicationTests
    Let's turn this thing into a real integration test

--- src/test/java/org/kraaknet/authenticarebankapi/AuthentiCareBankApiApplicationTests.java
+++ src/test/java/org/kraaknet/authenticarebankapi/AuthentiCareBankApiApplicationTests.java
@@ -1,12 +1,17 @@
 package org.kraaknet.authenticarebankapi;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static java.lang.Boolean.TRUE;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration
 class AuthentiCareBankApiApplicationTests {
 
     @Test
