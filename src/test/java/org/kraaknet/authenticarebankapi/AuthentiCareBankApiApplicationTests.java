5cd57d8edddd9b2898b169a209a13966d0a76e2b
Floris Kraak <floris.kraak@booking.com>
2023-10-22 17:38:50 +0200

    Implement first simple mockmvc tests for customer endpoints

--- src/test/java/org/kraaknet/authenticarebankapi/AuthentiCareBankApiApplicationTests.java
+++ src/test/java/org/kraaknet/authenticarebankapi/AuthentiCareBankApiApplicationTests.java
@@ -15,12 +15,15 @@ import org.springframework.web.context.WebApplicationContext;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.shaded.com.github.dockerjava.core.MediaType;
 import org.testcontainers.utility.DockerImageName;
 
 import static java.lang.Boolean.TRUE;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @ExtendWith(SpringExtension.class)
@@ -53,8 +56,16 @@ class AuthentiCareBankApiApplicationTests {
 
     @Test
     @WithMockUser(username = "john", roles = { "ADMIN" })
-    void givenRoleViewer_whenCallGetUsername_thenReturnUsername() {
+    void givenUserUnknown_whenCallGetCurrentCustomer_thenReturnNotFound() throws Exception {
+        mvc.perform(get("/customer/me").contentType(MediaType.APPLICATION_JSON.getMediaType()))
+                .andExpect(status().isNotFound());
+    }
 
+    @Test
+    @WithMockUser(username = "Floris")
+    void givenUserExists_whenCallGetCurrentCustomer_thenReturnCustomerDetails() throws Exception {
+        mvc.perform(get("/customer/me").contentType(MediaType.APPLICATION_JSON.getMediaType()))
+                .andExpect(status().isNotFound());
     }
 
 
