46f9889ad11d71dc103ece9aa5770406a24abdd2
Floris Kraak <floris.kraak@booking.com>
2023-10-23 12:40:45 +0200

    Fiddle with test code.
    I'm stuck. Security testing is painful, for no good reason. Why doesn't that post work??

--- src/test/java/org/kraaknet/authenticarebankapi/AuthentiCareBankApiApplicationTests.java
+++ src/test/java/org/kraaknet/authenticarebankapi/AuthentiCareBankApiApplicationTests.java
@@ -1,7 +1,5 @@
 package org.kraaknet.authenticarebankapi;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.Test;
@@ -30,7 +28,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.kraaknet.authenticarebankapi.AuthentiCareBankApiApplicationTests.UserRole.*;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.http.HttpMethod.GET;
-import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Slf4j
@@ -39,27 +36,14 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @Testcontainers
 class AuthentiCareBankApiApplicationTests {
 
-    private final ObjectWriter objectWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
-
     private final static HttpHeaders httpHeaders = createJsonRequestHeaders();
-    
+
     private static HttpHeaders createJsonRequestHeaders() {
         var requestHeaders = new HttpHeaders();
         requestHeaders.add(HttpHeaders.CONTENT_TYPE, APPLICATION_JSON_VALUE);
         return requestHeaders;
     }
 
-    public enum UserRole {
-        NONE,
-        USER,
-        ADMIN
-    }
-
-    private final static Map<UserRole, Pair<String, String>> roleToUserCredsMap = Map.of(
-            ADMIN, Pair.of("Floris", "wow_the_secret_password_sentence_is_very_long_indeed_but_of_course_its_compromised_it_should_not_be_stored_here"),
-            USER, Pair.of("Ramyaa", "aaymaR"),
-            NONE, Pair.of("FooUser", "")
-    );
 
     @Container
     @ServiceConnection
@@ -77,7 +61,7 @@ class AuthentiCareBankApiApplicationTests {
     }
 
     @Test
-    void givenNoUserWhenCallGetCurrentCustomerThenReturnRedirectionToLogin()  {
+    void givenNoUserWhenCallGetCurrentCustomerThenReturnRedirectionToLogin() {
         HttpEntity<Void> entity = new HttpEntity<>(httpHeaders);
         ResponseEntity<Void> result = testRestTemplate
                 .exchange("/customer/me", GET, entity, Void.class);
@@ -86,7 +70,7 @@ class AuthentiCareBankApiApplicationTests {
     }
 
     @Test
-    void givenAnonymousUserWhenCallGetCurrentCustomerThenGoKaboom()  {
+    void givenAnonymousUserWhenCallGetCurrentCustomerThenGoKaboom() {
         HttpEntity<Void> entity = new HttpEntity<>(httpHeaders);
         ResponseEntity<Void> result = restTemplateForRole(NONE)
                 .exchange("/customer/me", GET, entity, Void.class);
@@ -95,7 +79,7 @@ class AuthentiCareBankApiApplicationTests {
     }
 
     @Test
-    void givenUserUnknownWhenCallGetCurrentCustomerThenReturnNotFound()  {
+    void givenUserUnknownWhenCallGetCurrentCustomerThenReturnNotFound() {
         HttpEntity<Void> entity = new HttpEntity<>(httpHeaders);
         ResponseEntity<Void> result = restTemplateForRole(USER)
                 .exchange("/customer/me", GET, entity, Void.class);
@@ -104,20 +88,25 @@ class AuthentiCareBankApiApplicationTests {
     }
 
     @Test
-    void givenUserExistsWhenCallGetCurrentCustomerThenReturnCustomerDetails()  {
+    void givenUserExistsWhenCallGetCurrentCustomerThenReturnCustomerDetails() {
+        HttpEntity<Void> entity = new HttpEntity<>(httpHeaders);
+        ResponseEntity<Void> result = restTemplateForRole(USER)
+                .exchange("/customer/me", GET, entity, Void.class);
+        assertNotNull(result);
+        assertTrue(result.getStatusCode().isSameCodeAs(HttpStatusCode.valueOf(404)));
     }
 
     @Test
-    void givenUserExistsAndUserCustomerWhenCallGetCustomerByIdThenDenyAccess()  {
+    void givenUserExistsAndUserCustomerWhenCallGetCustomerByIdThenDenyAccess() {
     }
 
     @Test
-    void givenUserExistsAndUserAdminWhenCallGetCustomerByIdThenReturnCustomerDetails()  {
+    void givenUserExistsAndUserAdminWhenCallGetCustomerByIdThenReturnCustomerDetails() {
     }
 
 
     @Test
-    void givenAdminUserWhenCallCreateCustomerThenReturnNewCustomer()  {
+    void givenAdminUserWhenCallCreateCustomerThenReturnNewCustomer() {
         var newCustomer = CustomerModel.builder()
                 .userName("Floris")
                 .firstName("Floris")
@@ -125,9 +114,10 @@ class AuthentiCareBankApiApplicationTests {
                 .email("randakar@gmail.com")
                 .build();
         HttpEntity<CustomerModel> entity = new HttpEntity<>(newCustomer, httpHeaders);
-        TestRestTemplate template = restTemplateForRole(USER);
-        ResponseEntity<CustomerViewModel> result = template.exchange("/customer", POST, entity, CustomerViewModel.class);
+        TestRestTemplate template = restTemplateForRole(ADMIN);
+        ResponseEntity<CustomerViewModel> result = template.postForEntity("/customer", entity, CustomerViewModel.class);
         assertNotNull(result);
+        assertEquals(200, result.getStatusCodeValue());
         assertTrue(result.getStatusCode().is2xxSuccessful());
 
         var expectedResultBody = CustomerViewModel.builder()
@@ -143,13 +133,25 @@ class AuthentiCareBankApiApplicationTests {
         assertEquals(expectedResultBody, resultBody);
     }
 
+
+    public enum UserRole {
+        NONE,
+        USER,
+        ADMIN
+    }
+
+    private final static Map<UserRole, Pair<String, String>> roleToUserCredsMap = Map.of(
+            ADMIN, Pair.of("AlexanderKremer", "remerKrednaxelA"),
+            USER, Pair.of("Ramyaa", "aaymaR"),
+            NONE, Pair.of("FooUser", "")
+    );
+
     private TestRestTemplate restTemplateForRole(UserRole role) {
         Pair<String, String> creds = ofNullable(roleToUserCredsMap.get(role)).orElseThrow(AssertionError::new);
         String userName = creds.getLeft();
         String password = creds.getRight();
         return testRestTemplate
                 .withBasicAuth(userName, password);
-
     }
 
 
