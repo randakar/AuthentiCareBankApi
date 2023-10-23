759f16048143a242fad5c0681e28c84044e1e23d
Floris Kraak <floris.kraak@booking.com>
2023-10-23 21:52:47 +0200

    Fix imports

--- src/main/java/org/kraaknet/authenticarebankapi/repository/database/TransactionRepository.java
+++ src/main/java/org/kraaknet/authenticarebankapi/repository/database/TransactionRepository.java
@@ -1,10 +1,7 @@
 package org.kraaknet.authenticarebankapi.repository.database;
 
-import org.kraaknet.authenticarebankapi.repository.database.model.AccountEntity;
 import org.kraaknet.authenticarebankapi.repository.database.model.TransactionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<TransactionEntity, Long> {
 
